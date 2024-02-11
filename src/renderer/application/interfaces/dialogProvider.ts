cb67a7e90974edbc5303a917a71b09fd4d30bf8f
alexk111 <gh@alexkaul.com>
2024-02-11 22:15:32 +0400

    [renderer] Add file/dir dialog features

--- src/renderer/application/interfaces/dialogProvider.ts
+++ src/renderer/application/interfaces/dialogProvider.ts
@@ -3,7 +3,10 @@
  * GNU General Public License v3.0 or later (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import { MessageBoxConfig, MessageBoxResult } from '@common/base/dialog';
+import { MessageBoxConfig, MessageBoxResult, OpenDialogResult, OpenDirDialogConfig, OpenFileDialogConfig, SaveDialogResult, SaveFileDialogConfig } from '@common/base/dialog';
 export interface DialogProvider {
   showMessageBox: (config: MessageBoxConfig) => Promise<MessageBoxResult>;
+  showOpenFileDialog: (config: OpenFileDialogConfig) => Promise<OpenDialogResult>;
+  showSaveFileDialog: (config: SaveFileDialogConfig) => Promise<SaveDialogResult>;
+  showOpenDirDialog: (config: OpenDirDialogConfig) => Promise<OpenDialogResult>;
 }
--- src/renderer/infra/dialogProvider/osDialogProvider.ts
+++ src/renderer/infra/dialogProvider/osDialogProvider.ts
@@ -3,7 +3,7 @@
  * GNU General Public License v3.0 or later (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import { ipcShowOsMessageBoxChannel, IpcShowOsMessageBoxArgs, IpcShowOsMessageBoxRes } from '@common/ipc/channels';
+import { ipcShowOsMessageBoxChannel, IpcShowOsMessageBoxArgs, IpcShowOsMessageBoxRes, IpcShowOsOpenFileDialogArgs, IpcShowOsOpenFileDialogRes, ipcShowOsOpenFileDialogChannel, IpcShowOsSaveFileDialogArgs, IpcShowOsSaveFileDialogRes, ipcShowOsSaveFileDialogChannel, IpcShowOsOpenDirDialogArgs, IpcShowOsOpenDirDialogRes, ipcShowOsOpenDirDialogChannel } from '@common/ipc/channels';
 import { DialogProvider } from '@/application/interfaces/dialogProvider';
 import { electronIpcRenderer } from '@/infra/mainApi/mainApi';
 
@@ -12,6 +12,18 @@ export function createOsDialogProvider(): DialogProvider {
     showMessageBox: (config) => electronIpcRenderer.invoke<IpcShowOsMessageBoxArgs, IpcShowOsMessageBoxRes>(
       ipcShowOsMessageBoxChannel,
       config
-    )
+    ),
+    showOpenFileDialog: (config) => electronIpcRenderer.invoke<IpcShowOsOpenFileDialogArgs, IpcShowOsOpenFileDialogRes>(
+      ipcShowOsOpenFileDialogChannel,
+      config
+    ),
+    showSaveFileDialog: (config) => electronIpcRenderer.invoke<IpcShowOsSaveFileDialogArgs, IpcShowOsSaveFileDialogRes>(
+      ipcShowOsSaveFileDialogChannel,
+      config
+    ),
+    showOpenDirDialog: (config) => electronIpcRenderer.invoke<IpcShowOsOpenDirDialogArgs, IpcShowOsOpenDirDialogRes>(
+      ipcShowOsOpenDirDialogChannel,
+      config
+    ),
   }
 }
