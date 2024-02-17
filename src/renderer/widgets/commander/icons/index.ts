7b02a4dbbeda769f2d386452a88d62cbe24990b7
alexk111 <gh@alexkaul.com>
2024-02-17 21:30:40 +0400

    [commander] Setup UI

--- src/renderer/widgets/commander/icons/index.ts
+++ src/renderer/widgets/commander/icons/index.ts
@@ -3,8 +3,10 @@
  * GNU General Public License v3.0 or later (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
+import execCommandSvg from './exec-command.svg';
 import widgetSvg from './widget.svg';
 
 export {
+  execCommandSvg,
   widgetSvg
 }
--- src/renderer/widgets/commander/widget.tsx
+++ src/renderer/widgets/commander/widget.tsx
@@ -3,14 +3,21 @@
  * GNU General Public License v3.0 or later (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
  */
 
-import { ReactComponent, WidgetReactComponentProps } from '@/widgets/appModules';
+import { Button, ReactComponent, WidgetReactComponentProps } from '@/widgets/appModules';
 import { Settings } from './settings';
+import { execCommandSvg } from '@/widgets/commander/icons';
 
 function WidgetComp({settings}: WidgetReactComponentProps<Settings>) {
-  const {cwd} = settings;
-  return (
-    <>{cwd}</>
-  )
+  const {cmds} = settings;
+  return cmds.length>0
+    ? <Button
+        onClick={_ => {
+        }}
+        iconSvg={execCommandSvg}
+        title={`Execute Command-line${cmds.length>1 ? 's' : ''}`}
+        size='Fill'
+      />
+    : 'Command-lines not specified.';
 }
 
 export const widgetComp: ReactComponent<WidgetReactComponentProps<Settings>> = {
