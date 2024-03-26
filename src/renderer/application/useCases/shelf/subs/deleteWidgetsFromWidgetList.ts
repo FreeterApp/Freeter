c4552368b51fd3b5d29baa7b0e1a4d596b103415
alexk111 <gh@alexkaul.com>
2024-03-26 09:24:12 +0400

    Upd varname

--- src/renderer/application/useCases/shelf/subs/deleteWidgetsFromWidgetList.ts
+++ src/renderer/application/useCases/shelf/subs/deleteWidgetsFromWidgetList.ts
@@ -7,12 +7,12 @@ import { EntityId } from '@/base/entity';
 import { WidgetList } from '@/base/widgetList';
 
 export function deleteWidgetsFromWidgetListSubCase(
-  widgetIds: EntityId[],
+  widgetIdsToDel: EntityId[],
   widgetList: WidgetList,
 ): [newList: WidgetList, deletedIds: EntityId[]] {
   const delWidgetIds: EntityId[] = [];
   const newList = widgetList.filter(item => {
-    if (widgetIds.indexOf(item.widgetId) > -1) {
+    if (widgetIdsToDel.indexOf(item.widgetId) > -1) {
       delWidgetIds.push(item.widgetId);
       return false;
     } else {
