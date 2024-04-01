fd6b7b524b98c94a6b2ce7b252f5516706de87fa
alexk111 <gh@alexkaul.com>
2024-04-01 09:01:14 +0400

    [note] Make spell checking optional

---
+++ tests/renderer/widgets/note/fixtures.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright: (c) 2024, Alex Kaul
+ * GNU General Public License v3.0 or later (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+ */
+
+import { Settings } from '@/widgets/note/settings';
+
+export function fixtureSettings(settings: Partial<Settings>): Settings {
+  return {
+    spellCheck: true,
+    ...settings
+  }
+}
