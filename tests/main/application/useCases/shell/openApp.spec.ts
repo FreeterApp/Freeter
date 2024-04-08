5b01d8169d5bc4e3b3f0d62da7f1ae8916c63d8d
alexk111 <gh@alexkaul.com>
2024-04-08 18:42:59 +0400

    Fix tests

--- tests/main/application/useCases/shell/openApp.spec.ts
+++ tests/main/application/useCases/shell/openApp.spec.ts
@@ -4,14 +4,21 @@
  */
 
 import { createOpenAppUseCase } from '@/application/useCases/shell/openApp';
+import { ProcessInfo } from '@common/base/process';
 import { mockChildProcessProvider } from '@tests/infra/mocks/childProcessProvider';
+import { mockProcessProvider } from '@tests/infra/mocks/processProvider';
+import { fixtureProcessInfoLinux, fixtureProcessInfoMac, fixtureProcessInfoWin } from '@testscommon/base/fixtures/process';
 
-function setup() {
+function setup(processInfo: ProcessInfo) {
   const childProcessProviderMock = mockChildProcessProvider({
     spawnDetached: jest.fn()
   })
+  const processProviderMock = mockProcessProvider({
+    getProcessInfo: jest.fn(() => processInfo)
+  })
   const useCase = createOpenAppUseCase({
-    childProcessProvider: childProcessProviderMock
+    childProcessProvider: childProcessProviderMock,
+    processProvider: processProviderMock
   });
   return {
     childProcessProviderMock,
@@ -20,14 +27,34 @@ function setup() {
 }
 
 describe('openAppUseCase()', () => {
-  it('should call spawnDetached() of childProcessProvider with right params', () => {
+  it('should call spawnDetached() of childProcessProvider with right params, when os=linux', () => {
     const testPath = 'some/file/path'
     const testArgs = ['arg1', 'arg2']
-    const { useCase, childProcessProviderMock } = setup()
+    const { useCase, childProcessProviderMock } = setup(fixtureProcessInfoLinux())
 
     useCase(testPath, testArgs);
 
     expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledTimes(1);
     expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledWith(testPath, testArgs, { shell: false });
+  });
+  it('should call spawnDetached() of childProcessProvider with right params, when os=win', () => {
+    const testPath = 'some/file/path'
+    const testArgs = ['arg1', 'arg2']
+    const { useCase, childProcessProviderMock } = setup(fixtureProcessInfoWin())
+
+    useCase(testPath, testArgs);
+
+    expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledTimes(1);
+    expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledWith(testPath, testArgs, { shell: false });
+  });
+  it('should call spawnDetached() of childProcessProvider with right params, when os=mac', () => {
+    const testPath = 'some/file/path'
+    const testArgs = ['arg1', 'arg2']
+    const { useCase, childProcessProviderMock } = setup(fixtureProcessInfoMac())
+
+    useCase(testPath, testArgs);
+
+    expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledTimes(1);
+    expect(childProcessProviderMock.spawnDetached).toHaveBeenCalledWith('open', [testPath, '--args', ...testArgs], { shell: false });
   });
 })
